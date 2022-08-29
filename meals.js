76628794ece631a9b524f984f576dfc762198af4
Rajib Das <rajibrad@gmail.com>
2022-08-29 14:07:55 +0600

    complete

--- meals.js
+++ meals.js
@@ -7,7 +7,7 @@ const loadMeals = (search) => {
 };
 
 const displayMeals = (meals) => {
-  console.log(meals);
+  // console.log(meals);
   const mealsDiv = document.getElementById("meals");
   mealsDiv.innerHTML = "";
 
@@ -15,11 +15,11 @@ const displayMeals = (meals) => {
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
-              <div class="card">
+              <div class="card" onclick="loadMealDetails(${meal.idMeal})">
               <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text">${meal.strInstructions.slice(0, 50)}...</p>
+                <p class="card-text">${meal.strInstructions.slice(0, 20)}...</p>
               </div>
             </div>
  `;
@@ -32,6 +32,46 @@ const searchFood = () => {
   const searchValue = searchFood.value;
   loadMeals(searchValue);
   searchFood.value = "";
+};
+
+const loadMealDetails = (mealId) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+
+const displayMealDetails = (meal) => {
+  // console.log(meal);
+  const mealDetailsSection = document.getElementById("meal-details");
+  const mealDiv = document.createElement("div");
+  mealDetailsSection.innerHTML = "";
+  //   mealDiv.classList.add("row", "g-0");
+  mealDiv.innerHTML = `
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-4">
+            <img
+                src="${meal.strMealThumb}"
+                class="img-fluid rounded-start"
+                alt="..."
+            />
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">
+                    <small class="text-muted">country: ${meal.strArea}</small>
+                    <small class="text-muted">Category: ${meal.strCategory}</small>
+                    </p>
+                    <p class="card-text">${meal.strInstructions}</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>          
+  `;
+  mealDetailsSection.appendChild(mealDiv);
 };
 
 loadMeals("a");
