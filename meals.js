b383b67ea6f876e3fb28681aaee2253b096183b6
Rajib Das <rajibrad@gmail.com>
2022-08-29 15:12:14 +0600

    added modal for view details

--- meals.js
+++ meals.js
@@ -15,13 +15,23 @@ const displayMeals = (meals) => {
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
-              <div class="card" onclick="loadMealDetails(${meal.idMeal})">
-              <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text">${meal.strInstructions.slice(0, 20)}...</p>
+              <div class="card">
+                <img src="${
+                  meal.strMealThumb
+                }" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">${meal.strMeal}</h5>
+                  <p class="card-text">${meal.strInstructions.slice(
+                    0,
+                    20
+                  )}...</p>
+                </div>
+                <button onclick="loadMealDetails(${
+                  meal.idMeal
+                })" type="button" class="mt-3 w-75 mx-auto mb-2 btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  See Details
+                </button>
               </div>
-            </div>
  `;
     mealsDiv.appendChild(mealDiv);
   });
@@ -44,34 +54,52 @@ const loadMealDetails = (mealId) => {
 const displayMealDetails = (meal) => {
   // console.log(meal);
   const mealDetailsSection = document.getElementById("meal-details");
-  const mealDiv = document.createElement("div");
-  mealDetailsSection.innerHTML = "";
-  //   mealDiv.classList.add("row", "g-0");
-  mealDiv.innerHTML = `
-    <div class="card mb-3">
-        <div class="row g-0">
-            <div class="col-md-4">
-            <img
-                src="${meal.strMealThumb}"
-                class="img-fluid rounded-start"
-                alt="..."
-            />
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">${meal.strMeal}</h5>
-                    <p class="card-text">
-                    <small class="text-muted">country: ${meal.strArea}</small>
-                    <small class="text-muted">Category: ${meal.strCategory}</small>
-                    </p>
-                    <p class="card-text">${meal.strInstructions}</p>
-                    
-                </div>
-            </div>
+  const exampleModalLabel = document.getElementById("exampleModalLabel");
+  exampleModalLabel.innerText = `Details of ${meal.strMeal}`;
+  mealDetailsSection.innerHTML = `
+        <div class="card mx-auto" style="width: 25rem;">
+          <img src=${meal.strMealThumb} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">
+              <small class="text-muted">country: ${meal.strArea}</small>
+              <small class="text-muted">Category: ${meal.strCategory}</small>
+            </p>
+            <p class="card-text">${meal.strInstructions}</p>
+            
+          </div>
         </div>
-    </div>          
+  
   `;
-  mealDetailsSection.appendChild(mealDiv);
+  // mealDetailsSection.innerHTML = "";
+  // const mealDiv = document.createElement("div");
+
+  //   mealDiv.classList.add("row", "g-0");
+  // mealDiv.innerHTML = `
+  //   <div class="card mb-3">
+  //       <div class="row g-0">
+  //           <div class="col-md-4">
+  //           <img
+  //               src="${meal.strMealThumb}"
+  //               class="img-fluid rounded-start"
+  //               alt="..."
+  //           />
+  //           </div>
+  //           <div class="col-md-8">
+  //               <div class="card-body">
+  //                   <h5 class="card-title">${meal.strMeal}</h5>
+  //                   <p class="card-text">
+  //                   <small class="text-muted">country: ${meal.strArea}</small>
+  //                   <small class="text-muted">Category: ${meal.strCategory}</small>
+  //                   </p>
+  //                   <p class="card-text">${meal.strInstructions}</p>
+
+  //               </div>
+  //           </div>
+  //       </div>
+  //   </div>
+  // `;
+  // mealDetailsSection.appendChild(mealDiv);
 };
 
 loadMeals("a");
